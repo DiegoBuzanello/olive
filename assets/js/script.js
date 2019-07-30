67308b732548cf7e6ad0642c07197d364ca73c82
DiegoBuzanello <47098158+DiegoBuzanello@users.noreply.github.com>
2019-07-30 01:25:54 -0300

    30

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+function loaderA() {
+    document.getElementById('loader').style.zIndex = '-5' 
+    document.getElementById('overf').style.overflow = 'initial'
+    document.getElementById('background-main').style.zIndex="4"
+}
