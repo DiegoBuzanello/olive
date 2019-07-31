b915f4037447cadb0d9e8f35c3e4db9f9f339914
DiegoBuzanello <47098158+DiegoBuzanello@users.noreply.github.com>
2019-07-30 22:33:50 -0300

    h

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,11 @@
 
 function loaderA() {
-    document.getElementById('loader').style.zIndex = '-5' 
+    document.getElementById("loader").remove();
+    // document.getElementById('loader').style.zIndex = '-5' 
     document.getElementById('overf').style.overflow = 'initial'
     document.getElementById('background-main').style.zIndex="4"
     console.log('loader')
-    document.getElementById("loader").remove();
+   
     console.log('tirou')
 }
 function removeElement() {
