d4dfbf357d73a84c08dda01e18b3fd9d2b6abbaa
DiegoBuzanello <47098158+DiegoBuzanello@users.noreply.github.com>
2019-07-30 21:29:59 -0300

    r

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,8 +3,8 @@ function loaderA() {
     document.getElementById('overf').style.overflow = 'initial'
     document.getElementById('background-main').style.zIndex="4"
 }
-// function removeElement(overf) {
-//     // Removes an element from the document
-//     var element = document.getElementById('overf');
-//     element.parentNode.removeChild(element);
-// }
+function removeElement(overf) {
+    // Removes an element from the document
+    var element = document.getElementById('overf');
+    element.parentNode.removeChild(element);
+}
