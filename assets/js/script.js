142f36a9de8cc1461c1d47e37475f9be77e6cec9
DiegoBuzanello <47098158+DiegoBuzanello@users.noreply.github.com>
2019-07-30 22:26:59 -0300

    k

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,16 @@
+
 function loaderA() {
     document.getElementById('loader').style.zIndex = '-5' 
     document.getElementById('overf').style.overflow = 'initial'
     document.getElementById('background-main').style.zIndex="4"
+    console.log('loader')
+    document.getElementById("loader").remove();
+    console.log('tirou')
 }
 function removeElement() {
     // Removes an element from the document
-    var element = document.getElementById('loader');
+    console.log('remove')
+    var element = document.getElementById(loader);
     element.parentNode.removeChild(element);
+    console.log('remove')
 }
